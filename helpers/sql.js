7699a0ab4cd9a549037faebe1beed2bdce68c527
Zhenhua <zhenhua1109@yahoo.com>
2022-08-24 10:56:32 -0700

    add sqlToUpdate doc string

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,26 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+
+/** Given a user data object and an object of JS-to-SQL keys conversion,
+ * returns an object with keys of 'setCols' and 'values' where 'setCols'
+ * is a query string of columns to update for SQL and 'values' is an array
+ * of values to be updated for each corresponding column
+ *
+ * 'dataToUpdate' can include:
+ * { firstName, lastName, password, email, isAdmin }
+ *
+ * 'jsToSql' converts JS data keys to SQL column keys:
+    { firstName: "first_name",
+      lastName: "last_name",
+      isAdmin: "is_admin",
+    }
+ *
+ * Returns:
+    {
+      setCols: "first_name=$1 age=$2 ..."
+      values: ['Aliya', 32, ...]
+    }
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
@@ -8,7 +28,7 @@ function sqlForPartialUpdate(dataToUpdate, jsToSql) {
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
   const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+    `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
 
   return {
